abb6bf42d0c8335097a0bc7804cbb456f94d2198
Ajay Singh <78214430+Mr-Ajay-Singh@users.noreply.github.com>
2023-07-16 21:50:07 +0530

    Modified Current Scene, Added Proper Animation, Partial Initial item complete

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -1,35 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameControl : MonoBehaviour
 {
     
     [SerializeField]
-    private Transform[] pictures;
+    private GameObject parentObject;
 
     [SerializeField]
     private GameObject[] winText;
 
     public static bool youWin;
 
+    Transform[] childArray;
+
     void Start(){
+        childArray = parentObject.GetComponentsInChildren<Transform>(true);
+
         winText[0].SetActive(false);
         winText[1].SetActive(false);
+        int parentIndex = System.Array.IndexOf(childArray, parentObject.transform);
+        childArray = childArray.Where((_, index) => index != parentIndex).ToArray();
+
+        foreach (Transform child in childArray)
+        {
+            
+            float randomRotation = Random.Range(1, 4) * 90f;
+
+            Debug.Log("Start " + randomRotation);
+            child.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+        }
         youWin = false;
     }
 
     void Update(){
-        if(pictures[0].rotation.z == 0 &&
-        pictures[1].rotation.z == 0 &&
-        pictures[2].rotation.z == 0 &&
-        pictures[3].rotation.z == 0 
-        ){
-            youWin = true;
-            
+
+        foreach (Transform child1 in childArray)
+        {
+            float rotationZ = child1.rotation.eulerAngles.z;
+            if(Mathf.Abs(rotationZ) > 0.1f) return;
+        }
+        Debug.Log("Working");
+        youWin = true;
+
         winText[0].SetActive(true);
         winText[1].SetActive(true);
-        }
     }
     
     
---
+++ Assets/code/MainMenu/SettingCanvasClick1.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SettingCanvasClick1 : MonoBehaviour, IPointerClickHandler
+{
+
+    [SerializeField]
+    public Animator animator1;
+    [SerializeField]
+    public Animator animator2;
+
+    // Start is called before the first frame update
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        PlayAnimationAndContinue();
+    }
+
+    private IEnumerator PlayAnimationCoroutine()
+    {
+        // Play the animation
+
+        animator1.SetTrigger("run");
+        animator2.SetTrigger("run");
+
+        // Wait for the animation to complete
+        //while (animator1.GetCurrentAnimatorStateInfo(0).IsName("SlideInAnimation2") && animator1.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        //{
+        //    yield return null;
+        // }
+        //while (animator2.GetCurrentAnimatorStateInfo(0).IsName("SlideInAnimation") && animator2.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        //{
+        //    yield return null;
+        //}
+        yield return new WaitForSeconds(1f);
+
+        // Animation completed, continue with the function
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayAnimationAndContinue()
+    {
+        StartCoroutine(PlayAnimationCoroutine());
+    }
+
+    
+}
